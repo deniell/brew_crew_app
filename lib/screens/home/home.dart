852d372eecf43c0efea07d3391aad8a46413b171
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-02-11 15:55:35 +0200

    added signOut() method in AuthService class; added logic which return either the Home or Authenticate widget from Wrapper depends on auth state; updated home page design and added signOut button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,29 @@
+import 'package:brew_crew_app/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('home')
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.exit_to_app),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
