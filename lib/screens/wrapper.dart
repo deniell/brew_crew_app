3eca1b02a193dd62afe4fbd5ba6baf664c47c62b
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-02-11 15:35:43 +0200

    added provider package to dependencies; MaterialApp widget wraped with StreamProvider<User>; retrieve active user from Provider in Wrapper widget

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,16 @@
+import 'package:brew_crew_app/models/user.dart';
 import 'package:brew_crew_app/screens/authenticate/authenticate.dart';
 import 'package:brew_crew_app/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
+    final user = Provider.of<User>(context);
+    print(user);
+
     // return either the Home or Authenticate widget
     return Authenticate();
     
