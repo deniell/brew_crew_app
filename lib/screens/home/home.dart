115f53fa68c4c49da6c59bf7733fe6105c20b00e
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-06-21 23:16:19 +0300

    added Brew model; updated get brews stream; added method _brewListFromSnapshot(QuerySnapshot snapshot) to map create brew list from snapshot; BrewList updated

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,7 @@
+import 'package:brew_crew_app/models/brew.dart';
 import 'package:brew_crew_app/screens/home/brew_list.dart';
 import 'package:brew_crew_app/services/auth.dart';
 import 'package:brew_crew_app/services/database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>.value(
+    return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
