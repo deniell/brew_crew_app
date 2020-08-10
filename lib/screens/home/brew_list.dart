9fab290f6401dad9ac9736a67e00eb6c10831bdf
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-08-10 12:01:27 +0300

    added UserData class in user model; DatabaseService updated with get userData and UserData _userDataFromSnapshot(DocumentSnapshot snapshot) methods; Update button connected with Firebase on SettingsForm

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -12,20 +12,13 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
 
-    final brews = Provider.of<List<Brew>>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
 
-    if (brews != null)
-    {
-      return ListView.builder(
-        itemCount: brews.length,
-        itemBuilder: (context, index) {
-          return BrewTile(brew: brews[index]);
-        },
-      );
-    }
-    else
-    {
-      return Container();
-    }
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
